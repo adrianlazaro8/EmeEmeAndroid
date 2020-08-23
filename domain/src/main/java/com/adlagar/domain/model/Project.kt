39410236b81c7a0a24aad567d7e3fc6ec882b6ac
Adrian Lazaro <adri92_css@hotmail.com>
2020-08-23 12:44:22 +0200

    Project domain model added

---
+++ domain/src/main/java/com/adlagar/domain/model/Project.kt
@@ -0,0 +1,12 @@
+package com.adlagar.domain.model
+
+data class Project(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val createdDate: Long,
+    val thumbnail: String,
+    val featured: Boolean,
+    val city: String,
+    val images: List<ProjectImage>
+)
