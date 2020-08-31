0e5673abb7195ff3505b77875bdd016d84509208
Adrián Lázaro <adri92css@gmail.com>
2020-08-31 18:06:34 +0200

    FakeCompanyDataSource created

---
+++ app/src/main/java/com/adlagar/emeeme/data/FakeCompanyDataSource.kt
@@ -0,0 +1,15 @@
+package com.adlagar.emeeme.data
+
+import com.adlagar.data.source.CompanyRemoteDataSource
+import com.adlagar.domain.model.Contact
+
+class FakeCompanyDataSource: CompanyRemoteDataSource{
+
+    override suspend fun getContactInfo(): Contact {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAboutCompany(): String {
+        TODO("Not yet implemented")
+    }
+}
--- data/src/main/java/com/adlagar/data/source/CompanyRemoteDataSource.kt
+++ data/src/main/java/com/adlagar/data/source/CompanyRemoteDataSource.kt
@@ -3,6 +3,6 @@ package com.adlagar.data.source
 import com.adlagar.domain.model.Contact
 
 interface CompanyRemoteDataSource {
-    suspend fun getContactInfo(): List<Contact>
+    suspend fun getContactInfo(): Contact
     suspend fun getAboutCompany(): String
 }
