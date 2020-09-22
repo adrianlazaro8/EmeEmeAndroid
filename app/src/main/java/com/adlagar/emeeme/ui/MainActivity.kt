840cf62ec72022882663a8be911b16fb3066a25f
Adrián Lázaro <adri92css@gmail.com>
2020-09-22 23:23:11 +0200

    ApplicationComponent created on MainActivity

--- app/src/main/java/com/adlagar/emeeme/ui/MainActivity.kt
+++ app/src/main/java/com/adlagar/emeeme/ui/MainActivity.kt
@@ -2,21 +2,24 @@ package com.adlagar.emeeme.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.adlagar.emeeme.R
-import com.adlagar.emeeme.ui.about.AboutUsFragment
-import com.adlagar.emeeme.ui.contact.ContactFragment
-import com.adlagar.emeeme.ui.portfolio.PortfolioFragment
+import com.adlagar.emeeme.di.DaggerEmeEmeApplicationComponent
+import com.adlagar.emeeme.di.EmeEmeApplicationComponent
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var applicationComponent: EmeEmeApplicationComponent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        applicationComponent = DaggerEmeEmeApplicationComponent
+            .factory()
+            .create(application)
 
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
     }
