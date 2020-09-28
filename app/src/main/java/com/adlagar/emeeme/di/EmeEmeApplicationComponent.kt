984053b95d20ee5abf9ca6693e9279878cb93339
Adrián Lázaro <adri92css@gmail.com>
2020-09-28 23:48:54 +0200

    Added module for main component

--- app/src/main/java/com/adlagar/emeeme/di/EmeEmeApplicationComponent.kt
+++ app/src/main/java/com/adlagar/emeeme/di/EmeEmeApplicationComponent.kt
@@ -1,12 +1,15 @@
 package com.adlagar.emeeme.di
 
 import android.app.Application
+import com.adlagar.emeeme.ui.about.AboutUsViewModel
+import com.adlagar.emeeme.ui.contact.ContactViewModel
+import com.adlagar.emeeme.ui.portfolio.PortfolioViewModel
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [DataModule::class])
+@Component(modules = [DataModule::class, ViewModelsModule::class])
 interface EmeEmeApplicationComponent {
 
     @Component.Factory
