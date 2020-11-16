9906b819e2dfbed13297a501b1783ad14913d40f
Adrián Lázaro <adri92css@gmail.com>
2020-11-16 20:05:35 +0100

    filling projects with fake images

--- app/src/main/java/com/adlagar/emeeme/ui/portfolio/PortfolioViewModel.kt
+++ app/src/main/java/com/adlagar/emeeme/ui/portfolio/PortfolioViewModel.kt
@@ -5,33 +5,40 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.adlagar.domain.model.Project
+import com.adlagar.domain.model.ProjectImage
 import com.adlagar.usecases.GetAllProjectsUseCase
 import kotlinx.coroutines.launch
 
 class PortfolioViewModel(
     private val getAllProjectsUseCase: GetAllProjectsUseCase
-): ViewModel() {
+) : ViewModel() {
 
     private val _model = MutableLiveData<UiModel>()
     val model: LiveData<UiModel>
         get() {
-            if (_model.value == null) refresh()
+            if (_model.value == null) getProjects()
             return _model
         }
 
-    private fun getProjects() =  viewModelScope.launch {
-        getAllProjectsUseCase.invoke()
-    }
-
-    private fun refresh() {
+    private fun getProjects() {
         viewModelScope.launch {
-            _model.value = UiModel.Content(getAllProjectsUseCase.invoke())
+            val projects = getAllProjectsUseCase.invoke()
+            projects.map { fillProject(it) }
+            _model.value = UiModel.Content(projects)
         }
     }
 
     sealed class UiModel {
         object Loading : UiModel()
-        class Content(val movies: List<Project>) : UiModel()
+        class Content(projects: List<Project>) : UiModel()
     }
 
+    private fun fillProject(project: Project) {
+        project.thumbnail = "https://picsum.photos/200/300"
+        project.images = mutableListOf(
+            ProjectImage(1, "test", "https://picsum.photos/200/300"),
+            ProjectImage(2, "test2", "https://picsum.photos/200/300"),
+            ProjectImage(3, "test3", "https://picsum.photos/200/300")
+        )
+    }
 }
--- domain/src/main/java/com/adlagar/domain/model/Project.kt
+++ domain/src/main/java/com/adlagar/domain/model/Project.kt
@@ -5,10 +5,10 @@ data class Project(
     val title: String = "",
     val description: String = "",
     val createdDateMillis: Long = 0,
-    val thumbnail: String = "",
+    var thumbnail: String = "",
     val featured: Boolean = false,
     val city: String = "",
-    val images: List<ProjectImage> = mutableListOf(),
+    var images: List<ProjectImage> = mutableListOf(),
     val latitude: Double = 0.0,
     val longitude: Double = 0.0
 )
