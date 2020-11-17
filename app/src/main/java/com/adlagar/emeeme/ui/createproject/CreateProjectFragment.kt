1bbeab3719ba84c25ad7dd5f1d98aae8b65a33e2
Adrián Lázaro <adri92css@gmail.com>
2020-11-17 19:49:23 +0100

    Showing map inside CreateProjectFragment

--- app/src/main/java/com/adlagar/emeeme/ui/createproject/CreateProjectFragment.kt
+++ app/src/main/java/com/adlagar/emeeme/ui/createproject/CreateProjectFragment.kt
@@ -8,14 +8,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.adlagar.domain.model.Project
+import com.adlagar.emeeme.R
+import com.adlagar.emeeme.contact.GoogleMapUiSettingsCustomizer
 import com.adlagar.emeeme.databinding.FragmentCreateProjectBinding
 import com.adlagar.emeeme.ui.MainActivity
 import com.adlagar.emeeme.ui.extensions.getViewModelFactory
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import java.util.*
 
 class CreateProjectFragment : Fragment() {
 
     private var binding: FragmentCreateProjectBinding? = null
+
+    private lateinit var mapFragment: SupportMapFragment
 
     private val viewModel: CreateProjectViewModel by viewModels {
         getViewModelFactory { (activity as MainActivity).applicationComponent.createProjectViewModel }
@@ -31,6 +38,23 @@ class CreateProjectFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         createProjectButton()
+        mapFragment = childFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment
+        binding?.let {
+            mapFragment.getMapAsync { map ->
+                map.moveCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        LatLng(
+                            it.tvLatitude.text.toString().toDouble(),
+                            it.tvLongitude.text.toString().toDouble()
+                        ), 8F
+                    )
+                )
+                GoogleMapUiSettingsCustomizer(
+                    map
+                )
+            }
+        }
+
     }
 
     private fun createProjectButton() {
