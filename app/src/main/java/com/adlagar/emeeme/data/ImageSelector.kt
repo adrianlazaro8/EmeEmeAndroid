7c1799e4dd7042cc5b7701057a1168a07dbaf10d
Adrián Lázaro <adri92css@gmail.com>
2020-12-08 12:38:37 +0100

    Loading image AboutUs

---
+++ app/src/main/java/com/adlagar/emeeme/data/ImageSelector.kt
@@ -0,0 +1,40 @@
+package com.adlagar.emeeme.data
+
+import android.content.Context
+import android.content.Intent
+import androidx.fragment.app.Fragment
+import pl.aprilapps.easyphotopicker.*
+import java.io.File
+
+class ImageSelector(context: Context, private val fragment: Fragment) {
+    val easyImage: EasyImage = EasyImage.Builder(context) // Chooser only
+        .setChooserTitle("Elige imagen")
+        // Will tell chooser that it should show documents or gallery apps
+        //.setChooserType(ChooserType.CAMERA_AND_DOCUMENTS)  you can use this or the one below
+        .setChooserType(ChooserType.CAMERA_AND_GALLERY)
+        // Setting to true will cause taken pictures to show up in the device gallery, DEFAULT false
+        .setCopyImagesToPublicGalleryFolder(false) // Sets the name for images stored if setCopyImagesToPublicGalleryFolder = true
+        .setFolderName("EasyImage sample") // Allow multiple picking
+        .build()
+
+    fun selectImage() {
+        easyImage.openCameraForImage(fragment)
+    }
+
+    fun handleResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?,
+        imagePath: (File?) -> Unit
+    ) {
+        easyImage.handleActivityResult(requestCode, resultCode, data, fragment.requireActivity(), object: DefaultCallback(){
+            override fun onMediaFilesPicked(imageFiles: Array<MediaFile>, source: MediaSource) {
+                if(imageFiles.isNotEmpty()){
+                    imagePath.invoke(imageFiles[0].file)
+                } else {
+                    imagePath.invoke(null)
+                }
+            }
+        })
+    }
+}
--- app/src/main/java/com/adlagar/emeeme/ui/about/AboutUsFragment.kt
+++ app/src/main/java/com/adlagar/emeeme/ui/about/AboutUsFragment.kt
@@ -1,14 +1,20 @@
 package com.adlagar.emeeme.ui.about
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.adlagar.domain.model.AboutUs
+import com.adlagar.emeeme.data.ImageSelector
 import com.adlagar.emeeme.databinding.FragmentAboutUsBinding
 import com.adlagar.emeeme.ui.MainActivity
 import com.adlagar.emeeme.ui.extensions.getViewModelFactory
@@ -19,6 +25,10 @@ class AboutUsFragment : Fragment() {
     private var binding: FragmentAboutUsBinding? = null
 
     private var aboutUs: AboutUs? = null
+
+    private val imageSelector: ImageSelector by lazy {
+        ImageSelector(requireContext(), this)
+    }
 
     private val viewModel: AboutUsViewModel by viewModels {
         getViewModelFactory { (activity as MainActivity).applicationComponent.aboutUsViewModel }
@@ -38,7 +48,29 @@ class AboutUsFragment : Fragment() {
             binding.btAboutusContinue.setOnClickListener {
                 viewModel.modifyAboutCompany(aboutUs?.image.toString(), binding.etAboutus.text.toString())
             }
+
+            binding.ivAboutUs.setOnClickListener {
+
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED){
+                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA), 0);
+                } else {
+                    takePhoto()
+                }
+            }
         }
+    }
+
+    private fun takePhoto() {
+        imageSelector.selectImage()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imageSelector.handleResult(requestCode, resultCode, data){
+            binding?.ivAboutUs?.loadImage(it?.absolutePath.toString())
+        }
+
     }
 
     private fun updateUI(uiState: AboutUsViewModel.UiState) {
