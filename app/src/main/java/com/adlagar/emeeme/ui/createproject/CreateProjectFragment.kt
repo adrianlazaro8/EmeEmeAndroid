fc9a3e0bda0ce8e7bb7acc28c5bae1aa55820290
Adrián Lázaro <adri92css@gmail.com>
2021-03-07 19:14:09 +0100

    Selecting project image

--- app/src/main/java/com/adlagar/emeeme/ui/createproject/CreateProjectFragment.kt
+++ app/src/main/java/com/adlagar/emeeme/ui/createproject/CreateProjectFragment.kt
@@ -1,18 +1,25 @@
 package com.adlagar.emeeme.ui.createproject
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.adlagar.domain.model.Project
 import com.adlagar.emeeme.R
 import com.adlagar.emeeme.contact.GoogleMapUiSettingsCustomizer
+import com.adlagar.emeeme.data.ImageSelector
 import com.adlagar.emeeme.databinding.FragmentCreateProjectBinding
 import com.adlagar.emeeme.ui.MainActivity
 import com.adlagar.emeeme.ui.extensions.getViewModelFactory
+import com.adlagar.emeeme.ui.extensions.loadImage
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -26,6 +33,10 @@ class CreateProjectFragment : Fragment() {
 
     private val viewModel: CreateProjectViewModel by viewModels {
         getViewModelFactory { (activity as MainActivity).applicationComponent.createProjectViewModel }
+    }
+
+    private val imageSelector: ImageSelector by lazy {
+        ImageSelector(requireContext(), this)
     }
 
     override fun onCreateView(
@@ -58,6 +69,31 @@ class CreateProjectFragment : Fragment() {
                     showBottomSheetToSelectLocation(it)
                 }
             }
+        }
+
+        binding?.ivImagePicker?.setOnClickListener {
+
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ), 0);
+            } else {
+                takePhoto()
+            }
+        }
+
+    }
+
+    private fun takePhoto() {
+        imageSelector.selectImage()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imageSelector.handleResult(requestCode, resultCode, data){
+            binding?.ivImagePicker?.loadImage(it?.absolutePath.toString())
         }
 
     }
@@ -111,6 +147,8 @@ class CreateProjectFragment : Fragment() {
 
         }
     }
+
+
 
     companion object {
         private const val DEFAULT_LATITUDE = 39.46975
