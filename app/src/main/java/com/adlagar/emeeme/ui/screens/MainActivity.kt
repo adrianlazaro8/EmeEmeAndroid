ed34b667521bce6eb57bd1218fc118a34fb6a462
Adrián Lázaro <adri92css@gmail.com>
2021-03-14 18:42:34 +0100

    Synthetic removed

--- app/src/main/java/com/adlagar/emeeme/ui/screens/MainActivity.kt
+++ app/src/main/java/com/adlagar/emeeme/ui/screens/MainActivity.kt
@@ -2,25 +2,32 @@ package com.adlagar.emeeme.ui.screens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.adlagar.emeeme.R
+import com.adlagar.emeeme.databinding.ActivityMainBinding
 import com.adlagar.emeeme.di.DaggerEmeEmeApplicationComponent
 import com.adlagar.emeeme.di.EmeEmeApplicationComponent
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var applicationComponent: EmeEmeApplicationComponent
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         applicationComponent = DaggerEmeEmeApplicationComponent
             .factory()
             .create(application)
 
-        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
