265be98d53ca28341aaaf92e1a3ef78c904be75a
Adrián Lázaro <adri92css@gmail.com>
2021-03-22 19:36:26 +0100

    Updating projects with resized content images

--- app/src/main/java/com/adlagar/emeeme/ui/screens/projectcontent/ProjectContentViewModel.kt
+++ app/src/main/java/com/adlagar/emeeme/ui/screens/projectcontent/ProjectContentViewModel.kt
@@ -1,15 +1,21 @@
 package com.adlagar.emeeme.ui.screens.projectcontent
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.adlagar.data.source.ProjectsRemoteDataSource
 import com.adlagar.domain.model.Project
 import com.adlagar.usecases.UploadImageUseCase
+import id.zelory.compressor.Compressor
+import id.zelory.compressor.constraint.quality
+import id.zelory.compressor.constraint.resolution
 import kotlinx.coroutines.launch
 import java.io.File
 
 class ProjectContentViewModel(
+    private val projectsRemoteDataSource: ProjectsRemoteDataSource,
     private val uploadImageUseCase: UploadImageUseCase
 ) : ViewModel() {
 
@@ -17,7 +23,7 @@ class ProjectContentViewModel(
     val uiState: LiveData<UiState>
         get() = _uiState
 
-    fun uploadImage(
+    private fun uploadImage(
         project: Project,
         projectImage: File
     ) {
@@ -25,7 +31,7 @@ class ProjectContentViewModel(
             uploadImageUseCase.invoke(projectImage,
                 onSuccess = {
                     project.images.add(it)
-                    _uiState.value = UiState.ProjectUpdated(project)
+                    _uiState.value = UiState.ImageUploaded(project)
                 },
                 onFailure = {
                     _uiState.value = UiState.Error(it)
@@ -36,9 +42,24 @@ class ProjectContentViewModel(
         }
     }
 
-    sealed class UiState{
-        class Error(error: String): UiState()
-        class ProjectUpdated(project: Project): UiState()
+    fun updateProject(project: Project) {
+        viewModelScope.launch {
+            _uiState.value = UiState.ProjectUpdated(projectsRemoteDataSource.updateProject(project))
+        }
+    }
+
+    fun uploadResizedImage(context: Context, project: Project, file: File) = viewModelScope.launch {
+        val compressedFile = Compressor.compress(context, file) {
+            resolution(720, 480)
+            quality(80)
+        }
+        uploadImage(project, compressedFile)
+    }
+
+    sealed class UiState {
+        class Error(error: String) : UiState()
+        class ImageUploaded(val project: Project) : UiState()
+        class ProjectUpdated(project: Project) : UiState()
     }
 
 }
