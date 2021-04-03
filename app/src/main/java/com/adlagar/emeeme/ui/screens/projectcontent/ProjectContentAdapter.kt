aedf95cf832ca4ef86df32bec5263f90de299bef
Adrián Lázaro <adri92css@gmail.com>
2021-04-03 18:58:52 +0200

    Added files

---
+++ app/src/main/java/com/adlagar/emeeme/ui/screens/projectcontent/ProjectContentAdapter.kt
@@ -0,0 +1,31 @@
+package com.adlagar.emeeme.ui.screens.projectcontent
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.adlagar.emeeme.databinding.ProjectImageBinding
+import com.adlagar.emeeme.ui.common.extensions.loadImage
+
+class ProjectContentAdapter(
+    private val images: List<String>
+) : RecyclerView.Adapter<ProjectContentAdapter.ProjectContentViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectContentViewHolder {
+        val itemPortfolioBinding =
+            ProjectImageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ProjectContentViewHolder(itemPortfolioBinding)
+    }
+
+    override fun onBindViewHolder(holder: ProjectContentViewHolder, position: Int) {
+        holder.image.loadImage(images[position])
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    class ProjectContentViewHolder(projectImageBinding: ProjectImageBinding) :
+        RecyclerView.ViewHolder(projectImageBinding.root) {
+        val image = projectImageBinding.ivProjectImage
+    }
+}
