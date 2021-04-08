a8783b0a1d5f678e062a9d5b5a9913dbc59920a4
Adrián Lázaro <adri92css@gmail.com>
2021-04-08 21:16:49 +0200

    Improving dependencies imports on app build.gradle

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -1,27 +1,28 @@
 package dependencies
 
 object Dependencies {
-    val kotlin_standard_library = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin_version}"
-    val kotlin_core = "androidx.core:core-ktx:${Versions.core_ktx}"
-    val firebase_analytics = "com.google.firebase:firebase-analytics-ktx:${Versions.firebase_analytics}"
-    val firebase_firestore = "com.google.firebase:firebase-firestore:${Versions.firebase_firestore}"
-    val firebase_storage = "com.google.firebase:firebase-storage:${Versions.firebase_storage}"
-    val crashlytics = "com.google.firebase:firebase-crashlytics:${Versions.crashlytics}"
-    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
-    val dagger = "com.google.dagger:dagger:${Versions.dagger}"
-    val dagger_compiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
-    val navigation_fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
-    val navigation_ui = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
-    val maps = "com.google.android.gms:play-services-maps:${Versions.maps}"
-    val maps_ktx = "com.google.maps.android:maps-utils-ktx:${Versions.maps_ktx}"
-    val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
-    val viewmodels = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle_version}"
-    val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle_version}"
-    val compiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle_version}"
-    val legacy = "androidx.legacy:legacy-support-v4:${Versions.legacy}"
-    val lifecycle_extensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle_version}"
-    val easyimage = "com.github.jkwiecien:EasyImage:${Versions.easyimage_version}"
-    val kotlin_coroutines_play_services = "org.jetbrains.kotlinx:kotlinx-coroutines-play-services:${Versions.kotlin_coroutines_play_services}"
-    val image_compressor = "id.zelory:compressor:${Versions.image_compressor_version}"
-    val lottie = "com.airbnb.android:lottie:"
+
+    val values  = listOf(
+        "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin_version}",
+        "androidx.core:core-ktx:${Versions.core_ktx}",
+        "com.google.firebase:firebase-analytics-ktx:${Versions.firebase_analytics}",
+        "com.google.firebase:firebase-firestore:${Versions.firebase_firestore}",
+        "com.google.firebase:firebase-storage:${Versions.firebase_storage}",
+        "com.google.firebase:firebase-crashlytics:${Versions.crashlytics}",
+        "com.squareup.retrofit2:retrofit:${Versions.retrofit}",
+        "com.google.dagger:dagger:${Versions.dagger}",
+        "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}",
+        "androidx.navigation:navigation-ui-ktx:${Versions.navigation}",
+        "com.google.android.gms:play-services-maps:${Versions.maps}",
+        "com.google.maps.android:maps-utils-ktx:${Versions.maps_ktx}",
+        "com.github.bumptech.glide:glide:${Versions.glide}",
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle_version}",
+        "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle_version}",
+        "androidx.legacy:legacy-support-v4:${Versions.legacy}",
+        "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle_version}",
+        "com.github.jkwiecien:EasyImage:${Versions.easyimage_version}",
+        "org.jetbrains.kotlinx:kotlinx-coroutines-play-services:${Versions.kotlin_coroutines_play_services}",
+        "id.zelory:compressor:${Versions.image_compressor_version}",
+        "com.airbnb.android:lottie:${Versions.lottie_version}"
+    )
 }
