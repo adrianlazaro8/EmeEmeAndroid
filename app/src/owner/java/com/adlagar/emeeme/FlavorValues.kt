fc83f9e4cc112c83699286933f63c56618e8117d
Adrián Lázaro <adri92css@gmail.com>
2021-04-23 21:20:49 +0200

    Values added for owner

--- app/src/owner/java/com/adlagar/emeeme/FlavorValues.kt
+++ app/src/owner/java/com/adlagar/emeeme/FlavorValues.kt
@@ -7,5 +7,8 @@ class FlavorValues {
     companion object{
         val createProjectButtonVisibility = View.VISIBLE
         val projectContentImageVisibility = View.VISIBLE
+        val studyInfoSaveButtonVisibility = View.VISIBLE
+        val contactInfoSaveButtonVisibility = View.VISIBLE
+        val textsAreEditable = true
     }
 }
